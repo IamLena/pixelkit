a8861b69cd832075419f5bb286b589b77133c9c4
Lena <helly.luchina@gmail.com>
2021-02-12 16:06:18 +0300

    fixed device test assert

--- test_devicemanager.py
+++ test_devicemanager.py
@@ -10,13 +10,12 @@ def test_list_connected_devices():
 	'''
 	vDevices = [
 		SimpleNamespace(vid=None, pid=None, device="UNKNOWN"),
-		SimpleNamespace(vid=9025, pid=33102, device="MSK"),
 		SimpleNamespace(vid=1027, pid=24597, device="RPK")
 	]
 	with patch('communitysdk.RetailPixelKitSerial.serial_connect'),\
 		patch('serial.tools.list_ports.comports', return_value=vDevices):
 		devices = list_connected_devices()
-		assert len(devices) == 2
+		assert len(devices) == 1
 		for d in devices:
 			d.close()
 
@@ -25,18 +24,15 @@ def test_list_connected_devices_instances():
 	Should return connected instances of devices
 	'''
 	vDevices = [
-		SimpleNamespace(vid=9025, pid=33102, device="MSK"),
 		SimpleNamespace(vid=1027, pid=24597, device="RPK")
 	]
 	with patch('communitysdk.RetailPixelKitSerial.serial_connect'),\
 		patch('serial.tools.list_ports.comports', return_value=vDevices):
 		devices = list_connected_devices()
-		assert isinstance(devices[1], PixelKit)
+		assert isinstance(devices[0], PixelKit)
 		assert devices[0].is_connected == True
-		assert devices[1].is_connected == True
 		for d in devices:
 			d.close()
-
 
 test_list_connected_devices()
 test_list_connected_devices_instances()
