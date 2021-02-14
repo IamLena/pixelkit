6056bba1f7190be1f4733e9eb44d968931f69062
Lena <helly.luchina@gmail.com>
2021-02-14 17:22:31 +0300

    uncomment

--- c/use.c
+++ c/use.c
@@ -5,61 +5,61 @@
 #include <unistd.h>
 #include <stdio.h>
 
-// int set_interface_attribs (int fd, int speed, int parity)
-// {
-//         struct termios tty;
-//         if (tcgetattr (fd, &tty) != 0)
-//         {
-//                 error_message ("error %d from tcgetattr", errno);
-//                 return -1;
-//         }
+int set_interface_attribs (int fd, int speed, int parity)
+{
+        struct termios tty;
+        if (tcgetattr (fd, &tty) != 0)
+        {
+                printf("error %d from tcgetattr", errno);
+                return -1;
+        }
 
-//         cfsetospeed (&tty, speed);
-//         cfsetispeed (&tty, speed);
+        cfsetospeed (&tty, speed);
+        cfsetispeed (&tty, speed);
 
-//         tty.c_cflag = (tty.c_cflag & ~CSIZE) | CS8;     // 8-bit chars
-//         // disable IGNBRK for mismatched speed tests; otherwise receive break
-//         // as \000 chars
-//         tty.c_iflag &= ~IGNBRK;         // disable break processing
-//         tty.c_lflag = 0;                // no signaling chars, no echo,
-//                                         // no canonical processing
-//         tty.c_oflag = 0;                // no remapping, no delays
-//         tty.c_cc[VMIN]  = 0;            // read doesn't block
-//         tty.c_cc[VTIME] = 5;            // 0.5 seconds read timeout
+        tty.c_cflag = (tty.c_cflag & ~CSIZE) | CS8;     // 8-bit chars
+        // disable IGNBRK for mismatched speed tests; otherwise receive break
+        // as \000 chars
+        tty.c_iflag &= ~IGNBRK;         // disable break processing
+        tty.c_lflag = 0;                // no signaling chars, no echo,
+                                        // no canonical processing
+        tty.c_oflag = 0;                // no remapping, no delays
+        tty.c_cc[VMIN]  = 0;            // read doesn't block
+        tty.c_cc[VTIME] = 5;            // 0.5 seconds read timeout
 
-//         tty.c_iflag &= ~(IXON | IXOFF | IXANY); // shut off xon/xoff ctrl
+        tty.c_iflag &= ~(IXON | IXOFF | IXANY); // shut off xon/xoff ctrl
 
-//         tty.c_cflag |= (CLOCAL | CREAD);// ignore modem controls,
-//                                         // enable reading
-//         tty.c_cflag &= ~(PARENB | PARODD);      // shut off parity
-//         tty.c_cflag |= parity;
-//         tty.c_cflag &= ~CSTOPB;
-//         tty.c_cflag &= ~CRTSCTS;
+        tty.c_cflag |= (CLOCAL | CREAD);// ignore modem controls,
+                                        // enable reading
+        tty.c_cflag &= ~(PARENB | PARODD);      // shut off parity
+        tty.c_cflag |= parity;
+        tty.c_cflag &= ~CSTOPB;
+        // tty.c_cflag &= ~CRTSCTS;
 
-//         if (tcsetattr (fd, TCSANOW, &tty) != 0)
-//         {
-//                 error_message ("error %d from tcsetattr", errno);
-//                 return -1;
-//         }
-//         return 0;
-// }
+        if (tcsetattr (fd, TCSANOW, &tty) != 0)
+        {
+                printf ("error %d from tcsetattr", errno);
+                return -1;
+        }
+        return 0;
+}
 
-// void set_blocking (int fd, int should_block)
-// {
-//         struct termios tty;
-//         memset (&tty, 0, sizeof tty);
-//         if (tcgetattr (fd, &tty) != 0)
-//         {
-//                 error_message ("error %d from tggetattr", errno);
-//                 return;
-//         }
+void set_blocking (int fd, int should_block)
+{
+        struct termios tty;
+        memset (&tty, 0, sizeof tty);
+        if (tcgetattr (fd, &tty) != 0)
+        {
+                printf ("error %d from tggetattr", errno);
+                return;
+        }
 
-//         tty.c_cc[VMIN]  = should_block ? 1 : 0;
-//         tty.c_cc[VTIME] = 5;            // 0.5 seconds read timeout
+        tty.c_cc[VMIN]  = should_block ? 1 : 0;
+        tty.c_cc[VTIME] = 5;            // 0.5 seconds read timeout
 
-//         if (tcsetattr (fd, TCSANOW, &tty) != 0)
-//                 error_message ("error %d setting term attributes", errno);
-// }
+        if (tcsetattr (fd, TCSANOW, &tty) != 0)
+                printf ("error %d setting term attributes", errno);
+}
 
 int main (void) {
 	char *portname = "/dev/ttyUSB0";
@@ -67,19 +67,26 @@ int main (void) {
 	if (fd < 0)
 	{
 		printf("error %s\n", strerror(errno));
-		// error_message ("error %d opening %s: %s", errno, portname, strerror (errno));
+		// printf ("error %d opening %s: %s", errno, portname, strerror (errno));
 		return 1;
 	}
 
-	// set_interface_attribs (fd, B115200, 0);  // set speed to 115,200 bps, 8n1 (no parity)
-	// set_blocking(fd, 0);                // set no blocking
+	set_interface_attribs (fd, B115200, 0);  // set speed to 115,200 bps, 8n1 (no parity)
+	set_blocking(fd, 0);                // set no blocking
 
-	// write (fd, "hello!\n", 7);           // send 7 character greeting
+	write (fd, "hello!\n", 7);           // send 7 character greeting
 
-	// usleep ((7 + 25) * 100);             // sleep enough to transmit the 7 plus
-	// 									// receive 25:  approx 100 uS per char transmit
-	// char buf [100];
-	// int n = read (fd, buf, sizeof buf);
+	usleep ((7 + 25) * 100);             // sleep enough to transmit the 7 plus
+										// receive 25:  approx 100 uS per char transmit
+
+	char buf [100];
+	int n = read (fd, buf, sizeof buf);
+	printf("n = %d\n", n);
+	if (n != -1)
+	{
+		buf[n] = '\0';
+		printf("buf %s\n", buf);
+	}
 	close (fd);
 	return 0;
 }
